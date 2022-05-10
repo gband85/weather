5761b60cab5dc22ae3334002f790409da34ceb37
gband85 <garrett@gband.net>
2022-05-10 17:57:51 -0500

    add search suggestions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import { weatherUI } from './weatherUI'
-import { getWeather } from './weather'
+import { weatherUI,navbar } from './weatherUI'
+import { getCoords,getPlace } from './weather'
 import './style.css'
 
+document.body.appendChild(navbar())
 document.body.appendChild(weatherUI())
+document.querySelector("#location-search").addEventListener("keyup",function(e){
+  // console.log(e)
+  getCoords(e.target.value)
+})
+
 
    window.onload = () => {       
-     navigator.geolocation.getCurrentPosition(getWeather);
-     
+    navigator.geolocation.getCurrentPosition(function(position) { 
+      getPlace(position.coords.latitude,position.coords.longitude)
+    });
+  // console.log(navigator.geolocation)
    };
