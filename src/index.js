7125125357d90a80348df0ee958af04899bbbb37
gband85 <garrett@gband.net>
2022-05-15 21:16:29 -0500

    change id

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './style.css'
 
 document.body.appendChild(navbar())
 document.body.appendChild(weatherUI())
-document.querySelector("#location-search").addEventListener("keyup",function(e){
+document.querySelector("#location-input").addEventListener("keyup",function(e){
   // console.log(e)
   getCoords(e.target.value)
 })
